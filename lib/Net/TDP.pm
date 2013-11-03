fb1457fd37e4ed11fd1d6082a73066ce1b9c4d87
Jay Shirley <j@shirley.im>
2013-11-03 05:53:46 -0800

    Cleaning up, trucking along. More methods, all the time.

--- lib/Net/TDP.pm
+++ lib/Net/TDP.pm
@@ -38,15 +38,15 @@ has 'api_key' => (
 
 {
   no strict 'refs';
-  foreach my $write_method ( qw/create_goal/ ) {
+  foreach my $write_method ( qw/create_goal completed/) {
     *{$write_method} = sub {
-        shift->_spore->$write_method( payload => { @_ } )->body;
+        shift->_spore->$write_method( @_, payload => { @_ } )->body;
     };
   }
 
-  foreach my $read_method ( qw/list_goals/ ) {
+  foreach my $read_method ( qw/list_goals list_categories archive_goal/ ) {
     *{$read_method} = sub {
-      shift->_spore->$read_method->body;
+      shift->_spore->$read_method(@_)->body;
     };
   }
 }
@@ -77,6 +77,14 @@ sub _build_spec {
         "author"       : [ "J. Shirley <jshirley@tdp.me>" ],
         "meta"         : { "documentation" : "http://tech.tdp.me/clients/perl" },
         "methods"      : {
+            "list_categories" : {
+                "api_format"      : [ "json" ],
+                "path"            : "/category",
+                "method"          : "GET",
+                "expected_status" : [ 200 ],
+                "description"     : "Fetch all categories",
+                "authentication"  : true
+            },
             "list_goals" : {
                 "api_format"      : [ "json" ],
                 "path"            : "/goals",
@@ -89,19 +97,39 @@ sub _build_spec {
                 "api_format"      : [ "json" ],
                 "path"            : "/goals",
                 "method"          : "POST",
-                "expected_status" : [ 201 ],
+                "expected_status" : [ 201, 400 ],
                 "description"     : "Add a new goal",
                 "authentication"  : true,
                 "requires_params" : [
                   "name", "description", "color",
-                  "quantity", "cooldown",
+                  "quantity", "frequency",
                   "category_id"
                 ],
                 "optional_params": [
-                  "quantity_needed", "score",
+                  "quantity_needed", "score", "cooldown",
                   "require_note", "note_prompt",
                   "active", "public", "position"
                 ]
+            },
+            "completed" : {
+                "api_format"      : [ "json" ],
+                "path"            : "/goals/:id/completion",
+                "method"          : "POST",
+                "expected_status" : [ 201, 400 ],
+                "description"     : "Mark a goal as completed",
+                "authentication"  : true,
+                "requires_params" : [ "id" ],
+                "optional_params" : [ "date", "note", "quantity", "duration" ]
+            },
+            "archive_goal" : {
+                "api_format"      : [ "json" ],
+                "path"            : "/goals/:id",
+                "method"          : "DELETE",
+                "expected_status" : [ 200 ],
+                "description"     : "Archive a goal",
+                "authentication"  : true,
+                "requires_params" : [ "id" ],
+                "optional_params" : [ "permanent" ]
             }
         }
     });
--- live.pl
+++ live.pl
@@ -4,23 +4,61 @@ use strict;
 use warnings;
 
 use Try::Tiny;
+use DateTime;
+
 use Data::Printer;
 
 use Net::TDP;
 
-warn $ENV{'TDP_API_KEY'};
-
 my $tdp = Net::TDP->new(
   ( $ENV{'TDP_HOST'} ? ( base_url => $ENV{'TDP_HOST'} ) : () ),
   api_key  => $ENV{'TDP_API_KEY'},
+  # Turn on a whole lot of debugging
+  #debug    => 1
 );
+
+my $autoclean = 0;
+my $id = 0;
+
+# Not testing with a specific goal, create one for us to monkey with.
+unless ( $id ) {
+  my $categories = $tdp->list_categories->{categories};
+
+  my $status = $tdp->create_goal(
+    name        => 'Created from API',
+    description => 'From the API',
+    color       => "#336699",
+    quantity    => 1,
+    frequency   => 1,
+    category_id => $categories->[0]->{id}
+  );
+
+  p $status;
+  $id = $status->{id};
+  $autoclean = 1;
+}
+
+if ( $id ) {
+  print "Marking $id as completed!\n";
+  $tdp->completed(
+    id       => $id,
+    # Optional note attached to the record
+    note     => "note attached to completion record",
+    # The date to mark as done, iso8601 or a simple ymd is acceptable
+    date     => DateTime->now->subtract(days => 1)->iso8601,
+
+    # These fields are "in development", but they're still accessible.
+    # How many did we do? Keep this as one or you may break stuff :)
+    quantity => 1,
+    # How much time spent (in minutes)
+    duration => 45
+  );
+
+  if ( $autoclean ) {
+    print "Archiving goal $id\n";
+    p $tdp->archive_goal(id => $id);
+  }
+}
 
 p $tdp->list_goals;
 
-p $tdp->create_goal(
-  name => 'Created from API',
-  description => 'From the API',
-  color    => "#336699",
-  quantity => 1,
-  cooldown => 1,
-);
